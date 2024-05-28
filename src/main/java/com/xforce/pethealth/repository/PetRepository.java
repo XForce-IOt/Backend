24a440390a1197cb8a5b3dd86a8445072ff94b0f
Emerson <emersonquispe2808@gmail.com>
2024-05-28 15:58:17 -0500

    update pet-repository

--- src/main/java/com/xforce/pethealth/repository/PetRepository.java
+++ src/main/java/com/xforce/pethealth/repository/PetRepository.java
@@ -3,6 +3,9 @@ package com.xforce.pethealth.repository;
 import com.xforce.pethealth.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PetRepository extends JpaRepository<Pet, Long> {
+import java.util.List;
 
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    List<Pet> findByPetOwnerId(Long petOwnerId);
+    Pet findByNeckId(Long neckId);
 }
