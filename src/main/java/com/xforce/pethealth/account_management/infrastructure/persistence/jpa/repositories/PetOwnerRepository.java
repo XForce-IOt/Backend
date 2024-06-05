c99606a25545d01ef8a037e45cba84ddaf848e16
Emerson <emersonquispe2808@gmail.com>
2024-06-05 18:22:15 -0500

    update repositories

--- src/main/java/com/xforce/pethealth/account_management/infrastructure/persistence/jpa/repositories/PetOwnerRepository.java
+++ src/main/java/com/xforce/pethealth/account_management/infrastructure/persistence/jpa/repositories/PetOwnerRepository.java
@@ -1,9 +1,11 @@
 package com.xforce.pethealth.account_management.infrastructure.persistence.jpa.repositories;
 
-import com.xforce.pethealth.account_management.domain.model.entities.PetOwner;
+import com.xforce.pethealth.account_management.domain.model.aggregates.PetOwner;
+import com.xforce.pethealth.account_management.domain.model.entities.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PetOwnerRepository extends JpaRepository<PetOwner, Long> {
     boolean existsByEmail(String email);
-
 }
