a1f116b1bfac22f91070a1c3572f4ef14e97ab5c
Emerson <emersonquispe2808@gmail.com>
2024-06-05 18:21:44 -0500

    update services

--- src/main/java/com/xforce/pethealth/account_management/domain/services/PetOwnerCommandService.java
+++ src/main/java/com/xforce/pethealth/account_management/domain/services/PetOwnerCommandService.java
@@ -1,2 +1,18 @@
-package com.xforce.pethealth.account_management.domain.services;public class PetOwnerCommandService {
+package com.xforce.pethealth.account_management.domain.services;
+
+import com.xforce.pethealth.account_management.domain.model.commands.*;
+import com.xforce.pethealth.account_management.domain.model.aggregates.PetOwner;
+import com.xforce.pethealth.account_management.domain.model.entities.Pet;
+import com.xforce.pethealth.account_management.domain.model.entities.Subscription;
+
+import java.util.Optional;
+
+public interface PetOwnerCommandService {
+    Optional<PetOwner> handle(CreatePetOwnerCommand command);
+    Optional<PetOwner> handle(UpdatePetOwnerCommand command);
+    Optional<Pet> handle(AddPetCommand command);
+    void handle(DeletePetCommand command);
+    Optional<Pet> handle(UpdatePetCommand command);
+    Optional<Subscription> handle(AddSubscriptionCommand command);
+    Optional<Subscription> handle(UpdateSubscriptionCommand command);
 }
--- src/main/java/com/xforce/pethealth/account_management/domain/services/PetOwnerQueryService.java
+++ src/main/java/com/xforce/pethealth/account_management/domain/services/PetOwnerQueryService.java
@@ -1,2 +1,18 @@
-package com.xforce.pethealth.account_management.domain.services;public interface PetOwnerQueryService {
+package com.xforce.pethealth.account_management.domain.services;
+
+import com.xforce.pethealth.account_management.domain.model.aggregates.PetOwner;
+import com.xforce.pethealth.account_management.domain.model.entities.Pet;
+import com.xforce.pethealth.account_management.domain.model.entities.Subscription;
+import com.xforce.pethealth.account_management.domain.model.queries.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PetOwnerQueryService {
+    List<PetOwner> handle(GetAllPetOwnersQuery query);
+    Optional<PetOwner> handle(GetPetOwnerByIdQuery query);
+    List<Pet> handle(GetAllPetsQuery query);
+    Pet handle(GetPetByIdQuery query);
+    List<Pet> handle(GetAllPetsByNameQuery query);
+    Subscription handle(GetSubscriptionByIdQuery query);
 }
