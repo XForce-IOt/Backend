f093bbb6a4fbb4efa8876ec3579863880af08f02
Jesica Jaramillo A <81380838+Rut163@users.noreply.github.com>
2024-06-08 18:04:34 -0500

    Update SubscriptionResource.java

--- src/main/java/com/xforce/pethealth/account_management/interfaces/rest/resources/SubscriptionResource.java
+++ src/main/java/com/xforce/pethealth/account_management/interfaces/rest/resources/SubscriptionResource.java
@@ -2,5 +2,5 @@ package com.xforce.pethealth.account_management.interfaces.rest.resources;
 
 import com.xforce.pethealth.account_management.domain.model.value_objects.Plans;
 
-public record SubscriptionResource(Plans plans, Float price) {
+public record SubscriptionResource(Long id, Plans plans, Float price) {
 }
