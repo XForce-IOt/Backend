0ec43f150a8d22c73d8ed62956f79de7819d56b8
Leonardo Aquino <u20201b949@upc.edu.pe>
2024-06-08 15:06:29 -0500

    Update Clinic

--- src/main/java/com/xforce/pethealth/appointment_function/interfaces/rest/resources/ClinicResource.java
+++ src/main/java/com/xforce/pethealth/appointment_function/interfaces/rest/resources/ClinicResource.java
@@ -3,6 +3,7 @@ package com.xforce.pethealth.appointment_function.interfaces.rest.resources;
 public record ClinicResource(
         Long clinicId,
         double rating,
+        String name,
         String number,
         String socialMedia
 ) {
